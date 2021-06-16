0606396e0c632221560c3d0481e82d4e98b28b22
Ranga Senal Fernando <ranga.fernando@gmail.com>
2021-06-16 21:10:23 +1000

    Prefix list servie results to uniquely identify results

--- ListService/Controllers/ShopController.cs
+++ ListService/Controllers/ShopController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using System.Net.Http.Json;
+using System.Linq;
 
 namespace ListService.Controllers
 {
@@ -26,8 +27,9 @@ namespace ListService.Controllers
             _httpClient.BaseAddress = new Uri("http://shopservice-clusterip-srv:5002/");
             var res = await _httpClient.GetAsync("shop");
             if(res.IsSuccessStatusCode){
-                var list = await res.Content.ReadFromJsonAsync<List<string>>();
-                return Ok(list); 
+                var shops = await res.Content.ReadFromJsonAsync<List<string>>();
+                var shopWithPrefix = shops?.Select(i => $"from list service x : {i}").ToList();
+                return Ok(shopWithPrefix); 
             }
             return NotFound();
         }
