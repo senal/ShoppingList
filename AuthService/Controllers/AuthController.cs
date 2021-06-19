75f1577a0084cd8e10dc54eb1d596cc27c030927
Ranga Senal Fernando <ranga.fernando@gmail.com>
2021-06-19 14:35:04 +1000

    Add authentication service

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace  AuthService.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase 
+    {                             
+        public AuthController()
+        {
+            
+        }
+
+        [HttpGet]
+        public Task<string> Index()
+        {
+            return Task.FromResult("Huh... I'm the auth service");    
+        }
+    }
+}
