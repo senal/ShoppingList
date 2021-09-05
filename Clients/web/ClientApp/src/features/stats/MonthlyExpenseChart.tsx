1dd0a717ce6fc31a71aed5c16e155c00ef85ae4f
senal <ranga.fernando@gmail.com>
2021-09-05 22:41:55 +1000

    #3 Introduce bar chart to display monthly expenses stats

--- Clients/web/ClientApp/src/features/stats/MonthlyExpenseChart.tsx
+++ Clients/web/ClientApp/src/features/stats/MonthlyExpenseChart.tsx
@@ -1,17 +1,60 @@
 import React from 'react'
+import { BarChart, CartesianGrid, XAxis, YAxis, Bar } from 'recharts';
 
 import { IYearMonth } from '../navigation/IYearMonth';
+
 
 
 interface IMonthlyExpenseChartProps {
     yearMonth: IYearMonth
 } 
 
+
+interface IData {
+    name: string,
+    date: number,
+    total: number
+}
+
 const MonthlyExpenseChart = (props: IMonthlyExpenseChartProps) => {
 
     const {yearMonth} = props;
 
-    return (<div>Expense chart for the month : {yearMonth.year} - {yearMonth.month.text}</div>);
+    const data  = [
+        {
+            date: 31,
+            total: 240.89,
+            name: "sep-2"
+        },
+        {
+            date: 20,
+            total: 58.90,
+            name: "sep-3"
+        },
+        {
+            date: 10,
+            total: 120.00,
+            name: "sep-4"
+        },
+        {
+            date: 2,
+            total: 75.00,
+            name: "sep-5"
+        },
+    ] as IData []
+
+
+    return (<div>
+        <div>Expense chart for the month : {yearMonth.year} - {yearMonth.month.text}</div>
+        <div>
+        <BarChart width={730} height={250} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis dataKey="total" />
+            <Bar dataKey="total" fill="#8884d8" />
+        </BarChart>
+        </div>
+    </div>);
 }
 
 export default MonthlyExpenseChart;
