47c6d558c50104c21d7bae0984837b0ab5b5ab61
senal <ranga.fernando@gmail.com>
2021-09-07 22:58:12 +1000

    #7 Begin refactor domain

---
+++ List/Domain/Common/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Domain.Common
+{
+    public abstract class Entity
+    {
+
+        Guid _Id;
+        public virtual Guid Id
+        {
+            get
+            {
+                return _Id;
+            }
+            protected set
+            {
+                _Id = value;
+            }
+        }
+    }
+}
